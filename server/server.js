bd80df6ee8bb2b73ec0db296cd0225f8e8c1f4e8
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-07-08 16:08:28 -0400

    problem shooting errors w packages and removed routes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const {ApolloServer} = require("apollo-server-express");
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
-const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 
