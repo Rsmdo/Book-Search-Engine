0eea4ccc0ae0f09989c9f3276a804fc9599a8284
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-07-08 12:49:47 -0400

    added resolver

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,62 @@
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+    // get user by id
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findOne({ _id: context.user._id });
+        return user;
+      }
+    },
+  },
+//   mutations
+  Mutation: {
+    //   logiin amd signtoken implementation
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    //dsave and remove book aftre authg and login 
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { book }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: book } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: { bookId: bookId } } },
+        { new: true }
+      );
+      return updatedUser;
+    },
+  },
+};
+
+module.exports = resolvers;
