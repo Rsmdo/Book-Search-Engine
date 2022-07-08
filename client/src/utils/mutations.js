56ddee8ce416d7841bb9d8df62717894063e6dc8
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-07-08 14:36:08 -0400

    added mutation.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,66 @@
+import { gql } from '@apollo/client';
+//login for loginUser
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+// ADD_USER will execute the addUser mutation.
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// SAVE_BOOK will execute the saveBook mutation.
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+        pageCount
+        publishedDate
+      }
+    }
+  }
+`;
+// REMOVE_BOOK will execute the removeBook mutation.
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+        pageCount
+        publishedDate  
+      }
+    }
+  }
+`;
